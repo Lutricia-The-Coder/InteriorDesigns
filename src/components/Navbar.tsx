e856094968847ffa0de6baef462230774b54d944
KatXPanda <katlegoterrence5@gmail.com>
2026-07-22 11:22:03 +0200

    create nav bar and hero section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+
+import "../index.css";
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+
+            <div className="logo">
+                interior
+            </div>
+
+            <ul>
+
+                <li>Home</li>
+
+                <li>Links</li>
+
+                <li>About</li>
+
+            </ul>
+
+        </nav>
+    );
+}
