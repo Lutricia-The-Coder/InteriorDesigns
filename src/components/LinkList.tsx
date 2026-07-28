157cab55ad9b8d55ef7c64944b99749e674309ea
Lutricia-The-Coder <lutriciangomane.Zoe@gmail.com>
2026-07-28 08:45:17 +0200

    Implement edit and delete bookmark features

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -55,7 +55,7 @@ export default function LinkList({
         <LinkCard
           key={link.id}
           link={link}
-          onDelete={onDelete}
+          onDelete={onDelete} 
           onEdit={onEdit}
         />
       ))}
